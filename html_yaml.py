9d01305e533743568474f9fe253afe03bef91d91
Akhdan Dzaky Maulana <adzakymaulana15@gmail.com>
2023-06-04 15:18:49 +0900

    fetch elems

--- html_yaml.py
+++ html_yaml.py
@@ -16,6 +16,7 @@ from functools import reduce
 
 from langchain.document_loaders import UnstructuredHTMLLoader
 import requests
+import sys
 
 load_dotenv()
 
@@ -63,7 +64,6 @@ def generate_file_summary_yaml():
         page_content = file.page_content
         file_path = repo_name + str(i)
         print(file_path)
-        i += 1
 
         gen_file_path = f"./generated2/{repo_name}/file_summary/{file_path}.yaml"
 
@@ -72,16 +72,74 @@ def generate_file_summary_yaml():
             f.write(
                 chain.run({'file_path': file_path, 'page_content': page_content}))
             f.close()
+        i += 1
+
+
+def fetch_file_elements_yaml():
+    repo_name = "python_tutorial"
+    url = requests.get("https://www.w3schools.com/python/python_file_write.asp")
+    htmltext = url.text
+
+    f = open("temporaryfile.html", "w")
+    f.write(htmltext)
+    f.close()
+
+    loader = UnstructuredHTMLLoader("temporaryfile.html")
+    #loader = UnstructuredHTMLLoader("temporaryfile.html", mode="elements")
+
+    data = loader.load()
+
+    query = """
+    You are a professional Software Engineer who has scarce knowledge about E2E testing and file summarization.
+    Minimize the file into just its interactve elements and briefly describe their function.
+    In the generated yaml file, group the elements with similiar functions together in one section and include the section number and description.
+    If section have more than 5 elements, keep only 5 random elements.
+
+    Each section's format have to be like below, until triple dot (...):
+    
+    1. Section: ["section name"]
+       Elements: 
+        - [element1 : "element1 description"]
+        - [element2 : "element2 description"]
+    ...
+    
+    [File : {file_path}]
+    {page_content}
+
+    File Summary in yaml Format:
+    """
+    template = PromptTemplate(template=query, input_variables=[
+                              'file_path', 'page_content'])
+
+    openai = OpenAI(model_name="gpt-3.5-turbo")
+    chain = LLMChain(prompt=template, llm=openai)
+
+    i = 1
+    for file in data:
+        page_content = file.page_content
+        file_path = repo_name + str(i)
+        print(file_path)
+
+        gen_file_path = f"./generated2/{repo_name}/file_summary/{file_path}.yaml"
+
+        os.makedirs(os.path.dirname(gen_file_path), exist_ok=True)
+        with open(gen_file_path, "w") as f:
+            f.write(
+                chain.run({'file_path': file_path, 'page_content': page_content}))
+            f.close()
+        i += 1
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--generate_mode", "-m", type=str, required=True,
-                        help="Generate mode - summary-gen")
+                        help="Generate mode - summary-gen, fetch-elems.")
 
     args = parser.parse_args()
 
     if args.generate_mode == "summary-gen":
         generate_file_summary_yaml()
+    if args.generate_mode == "fetch-elems":
+        fetch_file_elements_yaml()
     else:
-        print("generate_mode(-m) option should be one of summary-gen. Please try again.")
+        print("generate_mode(-m) option should be one of summary-gen, fetch-elems. Please try again.")
